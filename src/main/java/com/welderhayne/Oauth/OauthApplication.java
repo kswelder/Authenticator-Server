f4cb7d47064a544c4defd39f288dd91de55feba7
kswelder <welderhayne2000@gmail.com>
2024-12-27 05:55:14 -0300

    Add configuration to Swagger in main application

--- src/main/java/com/welderhayne/Oauth/OauthApplication.java
+++ src/main/java/com/welderhayne/Oauth/OauthApplication.java
@@ -1,9 +1,18 @@
 package com.welderhayne.Oauth;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "OAuth Registration",
+				version = "1.0",
+				description = "Documentation to API OAuth Registration"
+		)
+)
 public class OauthApplication {
 
 	public static void main(String[] args) {
