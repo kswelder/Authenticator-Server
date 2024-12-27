8d2f2aaf8fc2db13ef5b660ae9ca02c2c411384d
kswelder <welderhayne2000@gmail.com>
2024-12-27 05:53:55 -0300

    A simple DTO to return validated Token

--- src/main/java/com/welderhayne/Oauth/Dtos/AuthorizationResponse.java
+++ src/main/java/com/welderhayne/Oauth/Dtos/AuthorizationResponse.java
@@ -1,4 +1,15 @@
 package com.welderhayne.Oauth.Dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorizationResponse {
+    private String username;
+    private Date expiration;
 }
