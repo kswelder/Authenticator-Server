84ab51bece3cdbfadcbdcd9fdb6f905c9263ae1f
kswelder <welderhayne2000@gmail.com>
2024-12-31 19:54:53 -0300

    Add new Route to admin controller no finished

--- src/main/java/com/welderhayne/Oauth/Controllers/AdminController.java
+++ src/main/java/com/welderhayne/Oauth/Controllers/AdminController.java
@@ -1,4 +1,23 @@
 package com.welderhayne.Oauth.Controllers;
 
+import com.welderhayne.Oauth.Dtos.RegisterDto;
+import com.welderhayne.Oauth.Services.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private RegisterService registerService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<RegisterDto>> getAll() {
+        return ResponseEntity.ok(registerService.getAllRegisters());
+    }
 }
