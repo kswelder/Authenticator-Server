5fa5f4d7f75dfa2a1dbea117ffacbfbfea68ad68
kswelder <welderhayne2000@gmail.com>
2025-01-04 03:59:46 -0300

    Add all methods to admin modelation Register, save, update, delete and change permition only ADMIN user.

--- src/main/java/com/welderhayne/Oauth/Controllers/AdminController.java
+++ src/main/java/com/welderhayne/Oauth/Controllers/AdminController.java
@@ -2,11 +2,10 @@ package com.welderhayne.Oauth.Controllers;
 
 import com.welderhayne.Oauth.Dtos.RegisterDto;
 import com.welderhayne.Oauth.Services.RegisterService;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,54 @@ public class AdminController {
     @GetMapping("/all")
     public ResponseEntity<List<RegisterDto>> getAll() {
         return ResponseEntity.ok(registerService.getAllRegisters());
+    }
+
+    @GetMapping("/find/register/id/{id}")
+    public ResponseEntity<RegisterDto> getRegisterById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(registerService.findById(id));
+    }
+
+    @GetMapping("/find/register/username/{username}")
+    public ResponseEntity<RegisterDto> getRegisterByUsername(@PathVariable("username") String username) {
+        return ResponseEntity.ok(registerService.findByUsername(username));
+    }
+
+    @GetMapping("/find/register/email/{email}")
+    public ResponseEntity<RegisterDto> getRegisterByEmail(@PathVariable("email") String email) {
+        return ResponseEntity.ok(registerService.findByEmail(email));
+    }
+
+    @PutMapping("/update/register")
+    public ResponseEntity<Void> updateRegister(
+            @NotBlank @RequestParam("id") String id,
+            @NotBlank @RequestParam("username") String username,
+            @NotBlank @RequestParam("email") String email
+    ) {
+        registerService.updateUser(id, username, email);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update/password")
+    public ResponseEntity<Void> updatePassword(
+            @NotBlank @RequestParam("id") String id,
+            @NotBlank @RequestParam("password") String password
+    ) {
+        registerService.updatePasswordRegister(id, password);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update/permition")
+    public ResponseEntity<Void> updatePermition(
+            @NotBlank @RequestParam("id") String id,
+            @NotBlank @RequestParam("perm") String perm
+    ) {
+        registerService.updatePermition(id, perm);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteRegister(@NotBlank @PathVariable("id") String id) {
+        registerService.deleteRegister(id);
+        return ResponseEntity.noContent().build();
     }
 }
