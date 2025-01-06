16656587db15e916c47ac675105107d476622ca4
kswelder <welderhayne2000@gmail.com>
2025-01-06 06:17:07 -0300

    Exception to throwable token invalid and xss vunerability

--- src/main/java/com/welderhayne/Oauth/Exceptions/XssCharTokenInvalidException.java
+++ src/main/java/com/welderhayne/Oauth/Exceptions/XssCharTokenInvalidException.java
@@ -1,4 +1,11 @@
 package com.welderhayne.Oauth.Exceptions;
 
-public class XssCharTokenInvalidException {
+public class XssCharTokenInvalidException extends IllegalArgumentException {
+    public XssCharTokenInvalidException(String message) {
+        super(message);
+    }
+
+    public XssCharTokenInvalidException(String message, Throwable error) {
+        super(message, error);
+    }
 }
