6720b98f5346f8e0ad97a25b472320fac688c4ab
kswelder <welderhayne2000@gmail.com>
2025-01-06 06:18:11 -0300

    get claims and check invalid tokens

--- src/main/java/com/welderhayne/Oauth/Services/ClaimService.java
+++ src/main/java/com/welderhayne/Oauth/Services/ClaimService.java
@@ -3,8 +3,11 @@ package com.welderhayne.Oauth.Services;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import com.welderhayne.Oauth.Dtos.AuthorizationResponse;
+import com.welderhayne.Oauth.Exceptions.XssCharTokenInvalidException;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClaimService {
@@ -19,5 +22,30 @@ public class ClaimService {
                 claimsSet.getSubject(),
                 claimsSet.getExpirationTime()
         );
+    }
+
+    public void sanatizationString(String input) {
+        char[] tokensInvalids = "<>;:`´&|^".toCharArray();
+        List<Character> findedInvalidTokens = List.of();
+
+        StringBuilder sanitized = new StringBuilder();
+
+        for (char c: input.toCharArray()) {
+            boolean isValid = true;
+
+            for (char invalid : tokensInvalids) {
+                if (c == invalid) {
+                    isValid = false;
+                    findedInvalidTokens.add(c);
+                }
+            }
+            if (isValid) {
+                sanitized.append(c);
+            }
+        }
+
+        if (findedInvalidTokens.size() > 0) {
+            throw new XssCharTokenInvalidException("Invalid tokens detected: " + findedInvalidTokens.stream().toString());
+        }
     }
 }
